2c9e3031ccd077b9ca7c41a40c00856acb144b9c
Marlin Schäfer <mb.schaefer@gmx.de>
2019-02-05 18:38:33 +0100

    Implemented the LaTeX-Code generation.

--- network.py
+++ network.py
@@ -2,6 +2,13 @@ import layers as l
 import sys
 import os
 
+def get_vertical_skip(name, prevNode, verticalSep):
+    ret = []
+    ret.append(('\\node[Dot] (' + str(name) + '_0) [below=' + str(verticalSep) + ' of ' + str(prevNode) + '] {};\n', None))
+    ret.append(('\\node[Dot] (' + str(name) + '_1) [below=1cm of ' + str(name) + '_0] {};\n', None))
+    ret.append(('\\node[Dot] (' + str(name) + '_2) [below=1cm of ' + str(name) + '_1] {};\n', None))
+    return((ret, str(name) + '_2'))
+
 class network(object):
     def __init__(self, layers=None, constructor=None):
         self.layer_counter = {}
@@ -90,14 +97,19 @@ class network(object):
         #Adjust the counting of layer types
         layer_type = layer.get_type()
         if layer_type in self.layer_counter:
+            print("Layer counter: {}".format(self.layer_counter[layer_type]))
             self.layer_counter[layer_type] += 1
         else:
+            print("Strating layer counter")
             self.layer_counter[layer_type] = 1
         
+        print("Pre setting: {}".format(self.layer_counter[layer_type]))
         #Set unique name
         if layer.name == layer_type:
+            print("In if")
             layer.set_name(layer_type + '_' + str(self.layer_counter[layer_type]))
         else:
+            print("In else")
             tmp_counter = 1
             for existing in self.layers:
                 if existing.name == layer.name:
@@ -120,6 +132,8 @@ class network(object):
         ret.append('\\begin{tikzpicture}[\n')
         ret.append('Dense/.style={circle, draw=black, very thick, minimum size=2cm},\n')
         ret.append('Conv1D/.style={circle, draw=black, very thick, minimum size=2cm},\n')
+        ret.append('VLineVertrex/.style={circle, draw=black, minimum size=0pt, inner sep=0pt},\n')
+        ret.append('Dot/.style={circle, draw=black, fill=black, minimum size=0.1cm, inner sep=0pt},\n')
         ret.append(']\n')
         ret.append('\n')
         return(ret)
@@ -128,33 +142,93 @@ class network(object):
         ret = []
         ret.append('\\end{tikzpicture}\n')
         ret.append('\\end{figure}\n')
+        return(ret)
+    
+    def get_gate(self, layer_code, LayerSpacing):
+        topNodeName = layer_code[-1][1][1]
+        botNodeName = layer_code[-1][-2][1]
+        numLayerSpacing = float(LayerSpacing[:-2])
+        
+        ret = []
+        ret.append(('%%Gate', None))
+        
+        ret.append(('\\node (gateLeftTop) [right=' + str(LayerSpacing) + ' of ' + topNodeName + '] {};\n', 'gateLeftTop'))
+        ret.append(('\\node (gateLeftBot) [right=' + str(LayerSpacing) + ' of ' + botNodeName + '] {};\n', 'gateLeftBot'))
+        ret.append(('\\path (gateLeftTop.base) -- node (gateLeftMid) {} (gateLeftBot.base) ;\n', 'gateLeftMid'))
+        
+        ret.append(('\n', None))
+        
+        ret.append(('\\node[Dot] (gateHDotLeft) [right=' + str(LayerSpacing) + ' of gateLeftMid] {};\n', 'gateHDotLeft'))
+        ret.append(('\\node[Dot] (gateHDotMid) [right=' + str(float(numLayerSpacing) / 2) + LayerSpacing[-2:] + 'cm of gateHDotLeft] {};\n', 'gateHDotMid'))
+        ret.append(('\\node[Dot] (gateHDotRight) [right=' + str(float(numLayerSpacing) / 2) + LayerSpacing[-2:] + 'cm of gateHDotMid] {};\n', 'gateHDotRight'))
+        
+        ret.append(('\n', None))
+        
+        ret.append(('\\node (gateRightTop) [right=' + str(numLayerSpacing * 3) + LayerSpacing[-2:] + ' of gateLeftTop] {};\n', 'gateRightTop'))
+        ret.append(('\\node (gateRightBot) [right=' + str(numLayerSpacing * 3) + LayerSpacing[-2:] + ' of gateLeftBot] {};\n', 'gateRightBot'))
+        
+        dr = []
+        dr.append('%%Draw Gate')
+        dr.append('\\draw (gateLeftTop.south) -- (gateLeftBot.north);\n')
+        dr.append('\\draw (gateRightTop.south) -- (gateRightBot.north);\n')
+        
+        return((ret, dr))
     
     def get_latex_lines(self, **kwargs):
         first_lines = self.get_first_lines()
         last_lines = self.get_last_lines()
-        raise NotImplementedError('This function does not yet work.')
-        sys.exit(1)
+        #raise NotImplementedError('This function does not yet work.')
+        #sys.exit(1)
         
-        core = []
+        nodes = []
+        draw = []
         
         if kwargs['ShowMaxFront'] + kwargs['ShowMaxEnd'] >= len(self.layers):
-            layers_front = self.layers
+            layers_before = self.layers
             layers_end = []
-        else
-            layers_front = self.layers[kwargs['ShowMaxFront']]
+        else:
+            layers_before = self.layers[kwargs['ShowMaxFront']]
             layers_end = self.layers[kwargs['ShowMaxEnd']]
         
-        for layer in layers_before:
-            core += layer.get_latex_lines(kwargs['ShowMaxTop'], kwargs['ShowMaxBottom'], kwargs['NeuronSpacing'], kwargs['MaxDepth'])
+        for idx, layer in enumerate(layers_before):
+            tmp_neurons, tmp_draw = layer.get_latex_lines(None if idx == 0 else (layers_before[idx-1].name + '_0'), kwargs['ShowMaxTop'], kwargs['ShowMaxBottom'], kwargs['NeuronSpacing'], kwargs['LayerSpacing'], kwargs['MaxDepth'])
+            nodes.append(tmp_neurons)
+            draw.append(tmp_draw)
+            
+        
+        drawGate = []
+        gate = []
         
         if not len(layers_end) == 0:
-            #TODO: Need two vertical lines with dots in between like to indicate network has more layers in between and to terminate lines.:
-            #   |     |
-            #   | ··· |
-            #   |     |
-            core += #Latex Code for the above
-            for layer in layers_end:
-                core += layer.get_latex_lines(kwargs['ShowMaxTop'], kwargs['ShowMaxBottom'], kwargs['NeuronSpacing'], kwargs['MaxDepth'])
+            gate, drawGate = self.get_gate(nodes, kwargs['LayerSpacing'])
+            for idx, layer in enumerate(layers_end):
+                tmp_neurons, tmp_draw = layer.get_latex_lines(layers_before[-1].name + '_0' if idx == 0 else (layers_end[idx-1].name + '_0'), kwargs['ShowMaxTop'], kwargs['ShowMaxBottom'], kwargs['NeuronSpacing'], kwargs['LayerSpacing'], kwargs['MaxDepth'])
+                nodes.append(tmp_neurons)
+                draw.append(tmp_draw)
+        
+        nodeCommands = []
+        drawCommands = []
+        for node in nodes[0]:
+            nodeCommands.append(node[0])
+        
+        for idx, drawLayer in enumerate(draw):
+            if idx < len(draw) - 1:
+                output_neurons = []
+                for node in nodes[idx+1]:
+                    if not node[1] == None:
+                        output_neurons.append(node[1])
+                    nodeCommands.append(node[0])
+                if len(output_neurons) == len(drawLayer):
+                    #Do Stuff
+                    for i, dl in enumerate(drawLayer):
+                        for command in dl:
+                            drawCommands.append(command[0] + output_neurons[i] + command[1])
+                else:
+                    raise RuntimeError('Ooops, something went wrong with the code. (network.get_latex_lines)')
+                    sys.exit(1)
+            
+        
+        core = nodeCommands + drawCommands
         
         return(first_lines + core + last_lines)
     
@@ -175,9 +249,9 @@ class network(object):
                 raise AttributeError("'to_latex' does not have an option '%s'" % str(key))
                 sys.exit(1)
         
-        lines = self.get_latex_lines(self, **opt_arg)
+        lines = self.get_latex_lines(**opt_arg)
         
-        if not os.path.isfile(file_name):
+        if not os.path.isfile(file_name) or True:
             with open(file_name, 'w') as FILE:
                 FILE.writelines(lines)
         else:
